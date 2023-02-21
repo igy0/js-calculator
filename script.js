24916f589ca51a43cc7bd7878f6826745179d5e7
Igy Lizardo <igylizardo@gmail.com>
2023-02-21 14:51:36 -0800

    added basic operations

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const container = document.querySelector('.container');
 let buttons = []
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 19; i++) {
     buttons[i] = document.createElement('button');
     buttons[i].classList.add(`button${i}`);
+    buttons[i].innerText = i;
     container.appendChild(buttons[i])
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+function sub(x, y) {
+    return x - y;
+}
+
+function mult(x, y) {
+    return x * y;
+}
+
+function div(x, y) {
+    return x / y;
 }
